a393ef24f3172c9204608cf1f5c1c9326a2b1b3e
Erick Rivera <yomellamoerick.771@gmail.com>
2025-01-12 03:19:51 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/frontend/FrontendFinalDrillingM6ErickriveraApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendFinalDrillingM6ErickriveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendFinalDrillingM6ErickriveraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpengaga/frontend/FrontendFinalDrillingM6ErickriveraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpengaga.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendFinalDrillingM6ErickriveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
